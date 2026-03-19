591b137186ec92fc2a49bf01eaa481b612a01cc2
Adao Pedro <adaopedro.pro@gmail.com>
2026-03-20 00:27:54 +0100

    adiciona funcao calcular

--- funcoes.php
+++ funcoes.php
@@ -2,3 +2,10 @@
 	function saudar(string $quem = "Desconhecido"): void {
 		echo "Ola, $quem";
 	}
+
+	function calcular(float $n1, float $n2, string $operacao = "+"): float {
+		if ($operacao === "+")
+			return $n1 + $n2;
+		else
+			"ERRO: operacao nao reconhecida";
+	}
